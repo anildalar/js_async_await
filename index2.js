49f7744ab018590b0b6e2c4af5c68c72a3ba814e
anildalar <dollorinfotech@gmail.com>
2021-06-07 09:04:16 +0530

    new

--- index2.js
+++ index2.js
@@ -8,10 +8,17 @@ function b(){
 
 async function a(){
     let po = await b();
+    /*
+    po.then((v)=>{
+        console.log('then '+v);
+    }).catch((v)=>{
+        console.log('catch '+v);
+    }).finally((v)=>{
+        console.log('finally '+v);
+    });
+    */
     console.log(po);
     console.log('ok');
     
 }
-
-
 a();
